520539cc48e6219a84cc908dd9eb0adc932727c6
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2023-10-05 10:28:41 +0100

    adding the navabar components to the hero

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,12 +1,12 @@
 "use client";
 import Image from 'next/image';
-import { CustomButtonProps } from './types';
+import { CustomButtonProps } from '../types';
 
-const CustomButton = ({ title, containerStyles, handleClick }: CustomButtonProps) => {
+const CustomButton = ({ title, containerStyles, handleClick, btnType }: CustomButtonProps) => {
   return (
     <button
       disabled={false}
-      type={"button"}
+      type={btnType || "button"}
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
--- components/index.tsx
+++ components/index.tsx
@@ -7,5 +7,5 @@ export {
   Hero,
   CustomButton,
   Navbar,
-  Footer
+  Footer,
 }
